ba8a56437367a1a393cb273fb8f7c53fb9e2dfce
Roman Hrabovyi <romansudova@gmail.com>
2024-05-22 20:40:10 +0300

    mod modal

--- js/script.js
+++ js/script.js
@@ -111,12 +111,15 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           modalClose = document.querySelector('[data-close]');
 
+    function openModal() {
+        modal.classList.add('show'),
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-            btn.addEventListener('click', () => {
-            modal.classList.add('show'),
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
+            btn.addEventListener('click',openModal);
     });
     
 
@@ -138,7 +141,17 @@ window.addEventListener('DOMContentLoaded', () => {
         if(e.code === 'Escape' && modal.classList.contains('show')) {
             closeModal();
         }
-    })
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener('scroll',showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll',showModalByScroll);
     
 });
 
